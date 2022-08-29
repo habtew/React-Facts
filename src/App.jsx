64b42582affdd83fc88d4eefe3ad7dbe35c8800a
Habtamu <habtamuwolde92@gmail.com>
2022-08-29 12:03:34 +0530

    complete react facts app built with react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import Nav from './components/Nav'
 import Component from './components/Component'
+import Footer from './components/Footer'
 function App(){
   return(
     <div>
       <Nav />
       <Component />
+      <Footer />
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
+import instalogo from '../images/instagram-Icon.png'
+import facebooklogo from '../images/facebook-Icon.png'
+import githublogo from '../images/gitHub-Icon.png'
 function Footer(){
     return(
         <div className="footer">
-            <p>hello</p>
+        <button type='button'><img src={instalogo} className="logo"/></button>
+        <button type='button'><img src={facebooklogo} className="logo" /></button>
+        <button type='button'><img src={githublogo} className="logo" /></button>
         </div>
     )
 }
+export default Footer;
